1ccccb91a9b37a1885a921e26d632d578f2e8e1b
jesale10 <alexis.ramirezn@proton.me>
2023-10-29 19:28:38 -0600

    Ended "Java BigDecimal" problem.

---
+++ src/Java_BigDecimal_Exercise/Solution.java
@@ -0,0 +1,29 @@
+package Java_BigDecimal_Exercise;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++){
+            s[i] = sc.next();
+        }
+        sc.close();
+
+        //Comparator for sorting BigDecimal strings in descending order.
+        Arrays.sort(s, Comparator.comparing(BigDecimal::new, Comparator.reverseOrder()));
+
+        //Output
+        System.out.println("\n");
+        System.out.println("Ordered:");
+        for (int i = 0; i < n; i++){
+            System.out.println(s[i]);
+        }
+    }
+}
